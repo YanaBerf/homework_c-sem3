1aa33f5bea9b7b7b64f92ff657284e3f1bdd5a0c
[YanaB] <[tudabae@gmail.com]>
2023-06-29 13:13:19 +0300

    куб числа от 1 до N

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+// while (i < n){
+   
+//     Console.WriteLine(i*i*i);
+//     i++;
+    
+// }Console.WriteLine(i*i*i);
+for (int i = 1; i <= n; i++){
+    Console.WriteLine(i*i*i);
+}
