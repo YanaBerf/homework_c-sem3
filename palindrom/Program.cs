e6a6944da39b0200155dcfb2dabd6ca9eff02151
[YanaB] <[tudabae@gmail.com]>
2023-06-29 12:26:05 +0300

    palindrom

---
+++ palindrom/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int n1 = num / 10000;
+int n2 = num / 1000 % 10;
+int n3 = num / 100 % 10;
+int n4 = num / 10 % 10;
+int n5 = num % 10;
+
+if (n1 == n5 && n2 == n4){
+    Console.WriteLine($"{num} -> да");
+}
+else {
+    Console.WriteLine($"{num} -> нет");
+}
+
